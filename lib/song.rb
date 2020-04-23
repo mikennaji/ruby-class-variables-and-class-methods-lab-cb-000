17f2e9d8ff42dcd28a20c4ed106613b74cbda173
Michael Nnaji <mnnaji3@uwo.ca>
2020-04-23 12:59:29 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,9 +1,11 @@
 class Song
   @@count = 0
   @@genres = []
+  @@artists = []
 
 def initialize(song, artist, genre)
     @@genres << genre
+    @@artists << artist
     @@count += 1
 end
 
@@ -11,7 +13,13 @@ def self.count
   @@count
 end
 
-
+def self.artists
+  @@artists.each_with_index do |word, index|
+    if @@gartists.count(word)>= 2
+      @@artists.delete_at(index)
+    end
+  end
+end
 
 def self.genres
   @@genres.each_with_index do |word, index|
